dd62fcb9f03104f9452e72ac0de6983d497ec429
unknown <zarghamkhan980@gmail.com>
2020-05-25 10:01:35 +0200

    Updates

---
+++ precache-manifest.28be556254afc2b4148b2527d535d4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d747e67c04345b4361841cbd432c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c771e20e13043d909e65",
+    "url": "/static/css/main.13b02f5d.chunk.css"
+  },
+  {
+    "revision": "ca1b9b235b2b705e4cfe",
+    "url": "/static/js/2.81e9aa4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.81e9aa4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c771e20e13043d909e65",
+    "url": "/static/js/main.40286743.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
